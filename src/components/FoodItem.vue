d8d6eff0ec4e4d177f96fd0bd7d9ef452030c3d9
Yassine <farid.yassine129@gmail.com>
2023-08-01 04:56:18 +0200

    handle user click on food item

--- src/components/FoodItem.vue
+++ src/components/FoodItem.vue
@@ -1,14 +1,23 @@
 <template>
-    <div>
-      <ul>
-              <li>{{ fooditem.name }}</li>
-              <li>{{ fooditem.description }}</li>
-              <li>{{ fooditem.price }}</li>
-      </ul>
-    </div>
-  </template>
-  
-  <script setup>
-    const { fooditem } = defineProps(['fooditem']);
-  </script>
-  
+  <div @click="handleClick">
+    <!-- Display the food item details here -->
+    {{ fooditem.name }}
+    {{ fooditem.description }}
+    {{ fooditem.price }}
+  </div>
+</template>
+
+<script setup>
+import { defineProps, defineEmits } from 'vue';
+
+// Declare the props using the `defineProps` function
+const { fooditem } = defineProps(['fooditem']);
+
+// Declare the emits using the `defineEmits` function
+const emit = defineEmits();
+
+const handleClick = () => {
+  // Emit the "select" event with the selected food item as the payload
+  emit('select', fooditem);
+};
+</script>
--- src/components/FoodType.vue
+++ src/components/FoodType.vue
@@ -3,7 +3,7 @@
     {{ foodtype.name }}
     <div v-if="foodtype.foodItems">
       <!-- Render the food items for the selected food type -->
-      <FoodItem v-for="fooditem in foodtype.foodItems" :key="fooditem.foodItemID" :fooditem="fooditem" />
+      <FoodItem v-for="fooditem in foodtype.foodItems" :key="fooditem.foodItemID" :fooditem="fooditem" @select="selectFoodItem" />
     </div>
   </div>
 </template>
@@ -15,9 +15,19 @@ import FoodItem from '../components/FoodItem.vue'; // Import the FoodItem compon
 // Declare the props using the `defineProps` function
 const { foodtype } = defineProps(['foodtype']);
 
+// Use ref() to create a reactive reference for selectedFoodItems
+const selectedFoodItems = ref([]);
+
+const selectFoodItem = (fooditem) => {
+  selectedFoodItems.value.push(fooditem); // Use selectedFoodItems.value to access the array
+  console.log(fooditem);
+};
+
 // Use the `onMounted` lifecycle hook to perform actions after the component is mounted
 onMounted(() => {
   // You can access the reactive foodtype reference using `foodtype`
   console.log(foodtype);
 });
+
+console.log(selectedFoodItems.value); // Use selectedFoodItems.value to access the array
 </script>
