7dcc60bf4b0ec8adf1b57b18bf49cf9bc0f88790
Yassine <farid.yassine129@gmail.com>
2023-08-13 17:57:17 +0200

    change minors

--- vite.config.js
+++ vite.config.js
@@ -13,20 +13,5 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  server: {
-    host: 'dev.local',
-    port: 8080,
-    https: {
-      key: fs.readFileSync('./certs/dev.local+4-key.pem'),
-      cert: fs.readFileSync('./certs/dev.local+4.pem'),
-      //ca: fs.readFileSync('./certs/my-ca.crt')
-    },
-    disableHostCheck: true,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:1337',
-        changeOrigin: true,
-      },
-    }
-  },
+ 
 })
